50ce2d28a8dbfa5c7530d498c1dfbb46f409af08
Emir <emirsavas@windowslive.com>
2021-01-18 00:24:20 +0300

    AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    
+    #region Singleton
+
+    public static AudioManager instance;
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
