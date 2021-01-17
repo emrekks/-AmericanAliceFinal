18a1ae31b3705f9317c8482e73e00212b01b36ec
Emir <emirsavas@windowslive.com>
2021-01-18 00:21:16 +0300

    UIManager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static UIManager instance;
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
