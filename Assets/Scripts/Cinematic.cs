d2cb7249f3f9a43d32e45e754ebe648a2881a5c1
Emir <emirsavas@windowslive.com>
2021-01-22 00:20:16 +0300

    Design Upgrades

---
+++ Assets/Scripts/Cinematic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Cinematic : MonoBehaviour
+{
+    public PlayerController _playerController;
+    public Camera mainCamera;
+    public GameObject cutsceneCamera;
+
+
+    void Start()
+    {
+        StartCoroutine(startCutscene());
+    }
+    
+
+    IEnumerator startCutscene()
+    {
+        mainCamera.enabled = false;
+        cutsceneCamera.SetActive(true);
+        yield return new WaitForSeconds(6.2f);
+        mainCamera.enabled = true;
+        cutsceneCamera.SetActive(false);
+    }
+
+}
+
