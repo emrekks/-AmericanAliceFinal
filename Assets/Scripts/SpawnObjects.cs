5f57c01225d25178c425f772369cce7de88c6ad3
Emir <emirsavas@windowslive.com>
2021-01-22 23:46:39 +0300

    NPC Finished

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,7 +7,7 @@ using UnityEngine.UI;
 
 public class SpawnObjects : MonoBehaviour
 {
-    
+
     public GameObject referance;
     public bool spawn = false;
     private float Timer = 0f;
@@ -14,6 +15,10 @@ public class SpawnObjects : MonoBehaviour
     public GameObject textScreen;
     public TextMeshProUGUI text;
     public bool firstCalled = false;
+    public bool level1 = true;
+    public bool level2 = false;
+    public bool npcLittle = false;
+    public LevelUp levelUp;
 
 
     void Start()
@@ -22,15 +27,15 @@ public class SpawnObjects : MonoBehaviour
     }
 
 
-     void Update()
+    void Update()
     {
-        
+
         if (spawn == true)
         {
             Timer += Time.deltaTime;
             textScreen.SetActive(true);
             cat.SetActive(true);
-            
+
             if (Timer >= 10f)
             {
                 cat.SetActive(false);
@@ -38,36 +43,73 @@ public class SpawnObjects : MonoBehaviour
                 Timer = 0f;
                 spawn = false;
             }
-            
+
         }
-        
-        
-        if(Input.GetKeyDown(KeyCode.C) && spawn == false) 
+
+
+        if (Input.GetKeyDown(KeyCode.C) && spawn == false)
         {
 
             cat.transform.position = referance.transform.position;
-            
-            if (firstCalled == true)
+
+            if (firstCalled == true && level1 == true && npcLittle == false && level2 == false)
             {
                 text.text = "Go to the portal and save us from queen's men.";
                 textScreen.SetActive(true);
                 spawn = true;
             }
-            else if(firstCalled == false)
+            else if (firstCalled == false && level1 == true && npcLittle == false)
             {
                 textScreen.SetActive(true);
                 firstCalled = true;
                 spawn = true;
             }
-            
-            
+
+            if (firstCalled == true && level2 == true && npcLittle == false)
+            {
+                text.text = "We are tired of both the queen and her men.";
+                textScreen.SetActive(true);
+                spawn = true;
+            }
+            else if (firstCalled == false && level2 == true && npcLittle == false)
+            {
+                textScreen.SetActive(true);
+                firstCalled = true;
+                spawn = true;
+            }
+
+            if (npcLittle == true)
+            {
+                text.text = "It is time to get smaller.";
+                textScreen.SetActive(true);
+                spawn = true;
+            }
+
         }
-        
+
     }
 
 
-     // void SpawnIt()
-     // {
-     //     Instantiate(cat, referance.transform.position, Quaternion.identity);
-     // }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("NpcLittle"))
+        {
+            npcLittle = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("NpcLittle"))
+        {
+            npcLittle = false;
+        }
+
+
+
+        // void SpawnIt()
+        // {
+        //     Instantiate(cat, referance.transform.position, Quaternion.identity);
+        // }
+    }
 }
