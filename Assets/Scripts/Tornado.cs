266a373fa58dabe8125bbecb5e186bbfd46652e8
Emir <emirsavas@windowslive.com>
2021-01-22 04:36:08 +0300

    Design Updates 5

---
+++ Assets/Scripts/Tornado.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tornado : MonoBehaviour
+{
+
+    public Animation anim;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim["Take 001"].speed = 0.1f;
+    }
+}
