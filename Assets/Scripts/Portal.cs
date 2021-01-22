c95325c88e8a559563a2de19d4b434529823ad49
Emir <emirsavas@windowslive.com>
2021-01-22 16:48:17 +0300

    New Level +Tutorial Level Finished

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = new Vector3(63, 17, -1300);
+        }
+    }
+}
