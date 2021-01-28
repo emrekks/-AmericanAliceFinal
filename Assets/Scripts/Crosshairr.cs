1f0e0f41eaf185857f7885e1e13b872f26895e77
Emir <emirsavas@windowslive.com>
2021-01-28 18:14:10 +0300

    Crosshair Script

---
+++ Assets/Scripts/Crosshairr.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshairr : MonoBehaviour
+{
+    public Vector3 rayhittransform;
+    public LayerMask rayMask;
+    public ParticleSystem _particle;
+    public GameObject crosshairObject;
+    public Camera cam;
+    private Vector3 up;
+    public WeaponController _weaponController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        crosshairObject.SetActive(false);
+        up = new Vector3(0, 0.34f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100, rayMask))
+        {
+            rayhittransform = hit.point;
+        }
+
+
+        if (hit.collider != null && !_weaponController.autoAimTrue)
+        {
+            crosshairObject.SetActive(true);
+            _particle.transform.position = up + rayhittransform;
+        }
+        else
+        {
+            crosshairObject.SetActive(false);
+        }
+
+    }
+}
